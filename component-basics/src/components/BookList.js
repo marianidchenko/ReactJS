447ee801b650594153ef6028c26dc9b3f48a71d2
Maria Nidchenko <marianidchenko@gmail.com>
2022-07-05 19:18:36 +0300

    updated to simpify book props

--- component-basics/src/components/BookList.js
+++ component-basics/src/components/BookList.js
@@ -1,16 +1,6 @@
 import { Book } from "./Book";
 
 export const BookList = (props) => {
-    const bookElements = props.books.map(book => (
-        <Book
-            title={book.title}
-            year={book.year_written}
-            price={book.price}
-            author={book.author}
-            edition={book.edition}
-        />
-    ));
-    return <ul>
-      {bookElements}
-    </ul>;
+    const bookElements = props.books.map((book) => <Book {...book} />);
+    return <ul>{bookElements}</ul>;
 };

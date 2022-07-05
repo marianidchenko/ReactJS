f771e4e2c823bb9d9889931f6a57493859838fdf
Maria Nidchenko <marianidchenko@gmail.com>
2022-07-05 19:16:42 +0300

    updated book display

--- component-basics/src/components/BookList.js
+++ component-basics/src/components/BookList.js
@@ -1,36 +1,16 @@
 import { Book } from "./Book";
 
 export const BookList = (props) => {
-  return (
-    <ul>
+    const bookElements = props.books.map(book => (
         <Book
-          title={props.books[0].title}
-          author={props.books[0].author}
-          year={props.books[0].year_written}
-          edition={props.books[0].edition}
-          price={props.books[0].price}
+            title={book.title}
+            year={book.year_written}
+            price={book.price}
+            author={book.author}
+            edition={book.edition}
         />
-      <Book
-        title={props.books[1].title}
-        author={props.books[1].author}
-        year={props.books[1].year_written}
-        edition={props.books[1].edition}
-        price={props.books[1].price}
-      />
-      <Book
-        title={props.books[2].title}
-        author={props.books[2].author}
-        year={props.books[2].year_written}
-        edition={props.books[2].edition}
-        price={props.books[2].price}
-      />
-      <Book
-        title={props.books[3].title}
-        author={props.books[3].author}
-        year={props.books[3].year_written}
-        edition={props.books[3].edition}
-        price={props.books[3].price}
-      />
-    </ul>
-  );
+    ));
+    return <ul>
+      {bookElements}
+    </ul>;
 };
